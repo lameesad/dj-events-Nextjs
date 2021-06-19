5051984c76d73d02f8ac3a1acc5780a94c6582d9
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-06-19 17:14:29 +0300

    Showcase Component

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import Header from './Header'
 import Footer from './Footer'
+import Showcase from './Showcase'
 import styles from '@/styles/Layout.module.css';
 
 export default function layout({ title, keywords, description, children }) {
+
+    const router = useRouter()
+
     return (
         <div>
             <Head>
@@ -13,6 +18,8 @@ export default function layout({ title, keywords, description, children }) {
                 <meta name="keywords" content={keywords} />
             </Head>
             <Header />
+
+            {router.pathname === '/' && <Showcase />}
             <div className={styles.container}>
                 {children}
             </div>

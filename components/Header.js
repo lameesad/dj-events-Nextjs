10ea35f8e396edce1cdcb9c5fae5e517091b1ec8
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-06-19 16:23:53 +0300

    layout, Header, Footer

---
+++ components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Link from 'next/link'
+import styles from '../styles/Header.module.css';
+
+
+export default function Header() {
+    return (
+        <div className={styles.header}>
+            <div className={styles.logo}>
+                <Link href='/'>
+                    <a>DJ Events</a>
+                </Link>
+            </div>
+
+            <nav>
+                <ul>
+                    <li>
+                        <Link href='/events'>
+                            <a>Events</a>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}

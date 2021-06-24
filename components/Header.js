4ecabe36ddf2e093ba0b0fbb606b979b9e3599ae
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-06-24 16:20:41 +0300

    Auth Context

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,12 @@
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa'
+import { useContext } from 'react'
 import Link from 'next/link'
 import Search from './Search'
 import styles from '../styles/Header.module.css';
-
+import AuthContext from '@/context/AuthContext'
 
 export default function Header() {
+    const { user, logout } = useContext(AuthContext)
     return (
         <div className={styles.header}>
             <div className={styles.logo}>
@@ -20,18 +22,41 @@ export default function Header() {
                             <a>Events</a>
                         </Link>
                     </li>
-                    <li>
-                        <Link href='/events/add'>
-                            <a>Add Event</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href='/account/login'>
-                            <a className='btn-secondary btn-icon'>
-                                <FaSignInAlt />Login
+                    {user ?
+                        // if logged in
+                        <>
+                            <li>
+                                <Link href='/events/add'>
+                                    <a>Add Event</a>
+                                </Link>
+                            </li>
+
+                            <li>
+                                <Link href='/account/dashboard'>
+                                    <a>Dashboard</a>
+                                </Link>
+                            </li>
+
+                            <li>
+                                <button onClick={() => logout()} className="btn-secondary btn-icon">
+                                    <FaSignOutAlt /> Logout
+                                </button>
+                            </li>
+
+                        </>
+                        :
+                        // if logged out
+                        <>
+                            <li>
+                                <Link href='/account/login'>
+                                    <a className='btn-secondary btn-icon'>
+                                        <FaSignInAlt />Login
                             </a>
-                        </Link>
-                    </li>
+                                </Link>
+                            </li>
+                        </>}
+
+
                 </ul>
             </nav>
         </div>
--- pages/account/login.js
+++ pages/account/login.js
@@ -4,7 +4,7 @@ import 'react-toastify/dist/ReactToastify.css'
 import { useState, useEffect, useContext } from 'react'
 import Link from 'next/link'
 import Layout from '@/components/Layout'
-// import AuthContext from '@/context/AuthContext'
+import AuthContext from '@/context/AuthContext'
 import styles from '@/styles/AuthForm.module.css'
 
 export default function LoginPage() {
@@ -12,13 +12,13 @@ export default function LoginPage() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    // const { login, error } = useContext(AuthContext)
+    const { login, error } = useContext(AuthContext)
 
-    // useEffect(() => error && toast.error(error))
+    useEffect(() => error && toast.error(error))
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        // login({ email, password })
+        login({ email, password })
     }
 
     return (

e3abc5e57a8f4293ae8ca7c22ef641c9798719e1
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-06-24 11:44:30 +0300

    Image Upload

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -9,7 +9,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import Layout from '@/components/Layout'
 import Modal from '@/components/Modal'
-// import ImageUpload from '@/components/ImageUpload'
+import ImageUpload from '@/components/ImageUpload'
 import { API_URL } from '@/config/index'
 import styles from '@/styles/Form.module.css'
 
@@ -68,6 +68,13 @@ export default function EditEventPage({ evt }) {
     const handleInputChange = (e) => {
         const { name, value } = e.target
         setValues({ ...values, [name]: value })
+    }
+
+    const imageUploaded = async (e) => {
+        const res = await fetch(`${API_URL}/events/${evt.id}`)
+        const data = await res.json()
+        setImagePreview(data.image.formats.thumbnail.url)
+        setShowModal(false)
     }
 
     return (
@@ -172,11 +179,11 @@ export default function EditEventPage({ evt }) {
 
             <Modal show={showModal} onClose={() => setShowModal(false)}>
                 IMAGE UPLOAD
-                {/* <ImageUpload
+                <ImageUpload
                     evtId={evt.id}
                     imageUploaded={imageUploaded}
-                    token={token}
-                /> */}
+
+                />
             </Modal>
         </Layout>
     )
